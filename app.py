e0616d71d06a1276f93f19e36966d7ae942eefbf
VitNog21 <victor210103@gmail.com>
2025-05-01 11:43:58 -0300

    Att 01/05/25

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ import sqlite3
 import webbrowser
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'
-
-
+# Detecta se está rodando como .exe (PyInstaller) ou normal
 if getattr(sys, 'frozen', False):
-    
     base_path = os.path.dirname(sys.executable)
 else:
-   
     base_path = os.path.dirname(__file__)
 
+# Caminho dos templates
+template_path = os.path.join(base_path, '_internal', 'templates')
+
+# Criação da aplicação Flask com template_folder definido
+app = Flask(__name__, template_folder=template_path)
+app.secret_key = 'sua_chave_secreta'
+
+# Caminho do banco de dados
 DB_PATH = os.path.join(base_path, 'Data', 'usuarios.db')
 
 def conectar_bd():
@@ -29,7 +32,6 @@ def login():
 
         conn = conectar_bd()
         cursor = conn.cursor()
-
         cursor.execute("SELECT * FROM Usuarios WHERE usuario = ? AND senha = ?", (usuario, senha))
         user = cursor.fetchone()
         conn.close()
@@ -53,15 +55,13 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
     if not os.path.exists(DB_PATH):
         print(f"❌ Banco de dados '{DB_PATH}' não encontrado.")
     else:
         import threading
-        
         def run():
             app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False) 
         t = threading.Thread(target=run)
         t.start()
-
         webbrowser.open("http://127.0.0.1:5000/")
