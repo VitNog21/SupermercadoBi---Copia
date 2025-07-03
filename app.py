3858188c2ca9148b293a179c097b75fad14f21b3
Victor Gabriel Nogueira Rocha <victor21@alu.ufc.br>
2025-07-03 16:07:58 -0300

    Atualização para testes

--- app.py
+++ app.py
@@ -6,13 +6,23 @@ import threading
 import time
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+template_dir = resource_path('templates')
+data_dir = resource_path('Data')
+DB_PATH = os.path.join(data_dir, 'usuarios.db')
+
+app = Flask(__name__, template_folder=template_dir)
 app.secret_key = 'sua_chave_secreta'
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(base_path, 'Data', 'usuarios.db')
-
 def conectar_bd():
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
     return sqlite3.connect(DB_PATH)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,13 +58,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    data_dir = os.path.dirname(DB_PATH)
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-
-    if not os.path.exists(DB_PATH):
-        print(f"AVISO: Banco de dados não encontrado em '{DB_PATH}'. Se for o primeiro uso, um será criado se necessário.")
-
     def run_app():
         app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
 
@@ -62,11 +65,9 @@ if __name__ == '__main__':
     flask_thread.daemon = True
     flask_thread.start()
 
-    time.sleep(1)
+    time.sleep(1.5)
     webbrowser.open("http://127.0.0.1:5000/")
 
-    try:
-        input("Servidor rodando. Pressione Enter para sair...\n")
-    except KeyboardInterrupt:
-        print("Servidor finalizado.")
-        
+    while True:
+        time.sleep(1)
+   
